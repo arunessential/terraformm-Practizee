206715dc33b8a36e7641958906029440521ec97a
arunrout-es <arunroutes@outlook.com>
2026-06-10 04:09:06 +0530

    RemoteDay-4

--- Terraform-Day4/main.tf
+++ Terraform-Day4/main.tf
@@ -8,3 +8,9 @@ resource "aws_subnet" "nuno-mendes-subnet" {
   vpc_id            = aws_vpc.nuno-mendes.id
   cidr_block        = "10.0.0.0/26"
 }
+
+# Create another subnet without overlaping
+resource "aws_subnet" "nuno-mendes-subnet2" {
+  vpc_id            = aws_vpc.nuno-mendes.id
+  cidr_block        = "10.0.0.64/26"
+}
