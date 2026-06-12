47f9b09d235add74f21a0750d57a6757df4105cc
arunrout-es <arunroutes@outlook.com>
2026-06-12 10:47:47 +0530

    updated with terraform.tfvars

--- Day5-remote-backend-statelock/backend.tf
+++ Day5-remote-backend-statelock/backend.tf
@@ -2,9 +2,18 @@ terraform   {
     backend "s3" {
         bucket = "statebucketdeployy"
         key    = "terraform.tfstate"
-        use_lockfile = true #s3 native locking foor state file to prevent concurent locking
+        dynamodb_table = "arunamsterdam"
+        encrypt = true
+       # s3 native locking foor state file to prevent concurent locking
+       # use_lockfile = true #s3 native locking foor state file to prevent concurent locking
         region = "us-east-1"
     }
 }
+
+
+#supports latest version >=1.10
+#<1.10 we can use dynamodb for state locking as well, but sw is more efficint 
+#State locking : Terraform acquires a state lock to protect the state from being written by multiple users at the same time. Please resolve the issue above and try again
+
 
 #State lockfile : Terraform acquires a state lock to protect the state from being written by multiple users at the same time. Please resolve the issue above and try again0
